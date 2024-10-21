927b6a448c86b2aae37cf3428e9cfd0fc06c66a4
Gax <gax.theodorio@crowdstrike.com>
2024-10-21 13:43:51 -0400

    Add python ttest code

--- examples/crowdstrike-python/__main__.py
+++ examples/crowdstrike-python/__main__.py
@@ -1,3 +1,13 @@
 """A Python Pulumi program"""
 
 import pulumi
+import crowdstrike_pulumi
+
+host_group = crowdstrike_pulumi.HostGroup(
+    resource_name="hostgroup_pulumi_python",
+    description="test pulumi provider",
+    type="dynamic",
+    assignment_rule="tags:'SensorGroupingTags/cloud-lab'+os_version:'Amazon Linux 2'",
+    
+)
+
